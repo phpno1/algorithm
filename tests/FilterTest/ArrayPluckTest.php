e167218a89f7421a2721039d6106206eee5f9269
ice.huang <ice.huang@wiwide.com>
2018-11-05 12:31:04 +0800

    修改注释

--- tests/FilterTest/ArrayPluckTest.php
+++ tests/FilterTest/ArrayPluckTest.php
@@ -1,12 +1,31 @@
 <?php
 
+/**
+ * @package    Phpno1\Algorithm\ArrayPluck
+ * @author     ice <354037968@@qq.com>
+ * @link       https://github.com/yb19890724
+ */
+
 namespace Phpno1\Test\FilterTest;
 
 use PHPUnit\Framework\TestCase;
 use Phpno1\Algorithm\Filter\ArrayPluck;
 
+/**
+ * Class ArrayPluckTest
+ *
+ *  多维数组扁平化一维度数组
+ *      数组内可嵌套多维数组
+ *      数组内可是字符串或数值
+ * @package Phpno1\Test\CalculationTest
+ * @author  ice <354037968@@qq.com>
+ */
+
 class ArrayPluckTest extends TestCase
 {
+    /**
+     * @var object $buyingCarCasesObject Phpno1\Algorithm\Filter\ArrayPluck
+     */
     private $arrayPluckObject;
 
     public function setUp()
